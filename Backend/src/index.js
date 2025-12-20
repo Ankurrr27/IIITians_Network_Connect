e9d045dfadac8422d5ee7f111ccd898eb1926741
Ankur <ankurp22singh@gmail.com>
2025-12-20 10:05:56 +0530

    added addres

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -24,12 +24,23 @@ import adminRoutes from "./routes/admin.routes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://iii-tians-network-connect-fg93.vercel.app"
+    ];
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
+
 
 app.use(express.json());
 
